627f80ae781f0e5d4f4e53fc8bd4140118c346f8
ka <624235036@parichat.skru.ac.th>
2021-10-03 14:32:38 +0700

    and

--- src/App.vue
+++ src/App.vue
@@ -37,8 +37,13 @@
 
     <div v-bind:key="emp.id" v-for="emp in employees">
       <b>Id</b>: {{emp.id}},<br/>
-      <b>Name</b>: {{emp.name}},<br/>
-      <b>Salary</b>: {{emp.Salary}}<br/><br/><br/>
+      <b>Name</b>: <span v-bind:class="(emp.Salary>90000) ? 'high' : (emp.Salary>=30000) ? 'medium' : 'low'">{{emp.name}}</span>,<br/>
+      <b>Salary</b>: {{emp.Salary}}
+      <span v-if="emp.Salary>90000">***</span>
+      <span v-else-if="emp.Salary>=30000">**</span>
+      <span v-else>*</span>
+      
+      <br/><br/><br/>
     </div>
     <hr/>
 
@@ -96,5 +101,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.high{
+  color: red;
+}
+.medium{
+  color: blue;
+}
+.low{
+  color: green;
 }
 </style>
